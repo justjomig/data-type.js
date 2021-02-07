6ac633c82e61ead448b80e8fcb711ae9de3ac1c0
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-07 17:44:25 +0800

    Push to github -initial commit

---
+++ data-types.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// String 
+const name = 'Just Jomig ';
+
+// Number or integer
+const age = 50;
+
+// Boolean
+const hasKids = false;
+
+// Null
+const car = null;
+
+// Undefined
+let test;
+
+
+// Reference Types - Object 
+const hobbies = ['watching', 'reading'];
+
+
+console.log(hobbies);
+
+// Object Literal 
+const address = {
+    city: 'Pasig',
+    street: 'Compton'
+}
+
+const today = new Date();
+console.log(today)
+console.log(typeof today);
+    
+
